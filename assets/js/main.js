1a0e5bb8c8954be927511ec226bbb5d6fbc21dd2
Benjamin Camacho <jahbenjah@hotmail.com>
2022-03-15 11:50:50 -0600

    fix property single carrousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -187,21 +187,6 @@
     }
   });
 
-  /**
-   * Property Single carousel
-   */
-  new Swiper('#property-single-carousel', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.property-single-carousel-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  
 
 })()
